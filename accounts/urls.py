7bc6a865d69e4f503e82ebcba0c2266ad4c9cde4
RuM <al.habiballah@tamkeentech.sa>
2022-04-18 21:33:28 +0300

    fix url & views

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup_view, name="signup")
+    path('signup/', views.signup_view, name="signup"),
+    path('login/', views.login_view, name="login")
 ]
