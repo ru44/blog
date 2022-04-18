e0ad876d6154e6070e5412d66bbcc4f2c03fa727
RuM <al.habiballah@tamkeentech.sa>
2022-04-18 15:10:57 +0300

    login

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def signup_view(request):
+    if request.method == 'POST':
+         form = UserCreationForm(request.POST)
+         if form.is_valid():
+             form.save()
+             #  log the user in
+             return redirect('article:list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', { 'form': form })
--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,3 @@
-from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -9,7 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
     path('', views.home),
-    path('art/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('art/', include('articles.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
